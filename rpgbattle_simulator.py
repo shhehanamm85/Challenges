a933bae408e86694a186ca848292cb92d53e0bd0
shhehanamm85 <145649847+shhehanamm85@users.noreply.github.com>
2025-12-21 17:42:28 +0530

    Add files via upload

--- rpgbattle_simulator.py
+++ rpgbattle_simulator.py
@@ -8,33 +8,31 @@ while True:
     print(player)
     enemy='attack'
     print(enemy)               
-    if E_health < 0 :
+    if E_health <=0 :
         print("Player wins")
         break
-    if p_health <0:
+    if p_health <=0:
         print("Enemy wins")
         break
     if player=="attack":
         p_score=random.randint(10,30)
         E_health-=p_score
-        print(f'player:-{p_health}\n enemy:-{E_health}')
+        print(f'player:-{p_health}\n enemy reduced attack:-{E_health}')
     elif  player=='defend' :
                      enemy_damage=random.randint(10,35)
                      e_score=enemy_damage//2
                      p_health-=e_score
-                     print(f'{p_health},{e_score}')
+                     print(f' player damage reduced to {e_score},player health {p_health}')
     elif player=="heal":  
                     p_score=random.randint(10,25) 
                     p_health+=p_score
+                    print(f"heal :-{p_health}")
                     if  p_health >100:
-                            p_health=100
+                        p_health=100
                     enemy_damage = random.randint(10,35)
                     p_health -= enemy_damage
-                    print(f'player:-{p_health}\n enemy:-{E_health}')   
-                                                   
-    else:
-                        print("Restart again")
-
+                    print(f'player damage :-{p_health}\n enemy:-{E_health}')   
+   
 
 
      
